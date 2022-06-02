60148c8ad498623e7f54121a87e37574e1f59b1a
sergeedouard <sjoseph.eng@gmail.com>
2022-06-02 09:45:12 -0400

    webpack and package JSON adjusted for Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,28 @@
 const path = require("path");
 
-module.exports = {};
+module.exports = {
+  context: __dirname,
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", "*"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          // query: {
+          //   presets: ["@babel/env", "@babel/react"],
+          // },
+        },
+      },
+    ],
+  },
+  devtool: "source-map",
+};
